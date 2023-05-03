bc7397cafe25b556283e4b558b9f909929af20b6
polinabubb <polinabubb@gmail.com>
2023-05-04 03:36:45 +0500

    жоский html+css

--- tasks.js
+++ tasks.js
@@ -2,7 +2,6 @@ document.querySelector('.task_table').addEventListener('click', function (e) {
     let modal = document.getElementById('modal');
     modal.classList.add('modal_active');
 });
-
 document.querySelector('.modal__close-button').addEventListener('click', function (e) {
     let modal = document.getElementById('modal');
     modal.classList.remove('modal_active');
