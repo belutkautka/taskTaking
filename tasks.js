a1aa4241c81b060a612b9954d39de1461833bec5
polinabubb <polinabubb@gmail.com>
2023-05-03 13:56:32 +0500

    lightbox + tasks

---
+++ tasks.js
@@ -0,0 +1,9 @@
+document.querySelector('.task_table').addEventListener('click', function (e) {
+    let modal = document.getElementById('modal');
+    modal.classList.add('modal_active');
+});
+
+document.querySelector('.modal__close-button').addEventListener('click', function (e) {
+    let modal = document.getElementById('modal');
+    modal.classList.remove('modal_active');
+})
