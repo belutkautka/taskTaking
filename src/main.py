e894d71554d8427c839d117c208cddc0df021454
JustASpace <js4std@gmail.com>
2023-05-06 17:12:31 +0500

    add some!

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+from src.pages.router import router as router_pages
+
+app = FastAPI()
+app.include_router(router_pages)
+app.mount("/static", StaticFiles(directory='src/static'), name="static")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
---
+++ src/pages/router.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+
+# from operations.router import get_specific_operations
+# uvicorn src.main:app --reload
+router = APIRouter(
+    prefix="/pages",
+    tags=["Pages"]
+)
+
+templates = Jinja2Templates(directory="src/templates")
+
+
+@router.get("/notifications")
+def get_base_page(request: Request):
+    return templates.TemplateResponse("notifications.html", {"request": request})
+
+
+@router.get("/profile")
+def get_base_page(request: Request):
+    return templates.TemplateResponse("profile.html", {"request": request})
+
+
+@router.get("/startpage")
+def get_base_page(request: Request):
+    return templates.TemplateResponse("startPage.html", {"request": request})
+
+
+@router.get("/tasks")
+def get_base_page(request: Request):
+    return templates.TemplateResponse("tasks.html", {"request": request})
+
+
+# @router.get("/search/{operation_type}")
+# def get_search_page(request: Request, operations=Depends(get_specific_operations)):
+#     return templates.TemplateResponse("search.html", {"request": request, "operations": operations["data"]})
