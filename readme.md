3c233761641abc72bce65735c06994324038dc56
JustASpace <js4std@gmail.com>
2023-05-18 03:31:55 +0500

    readme + теперь сразу перенаправляется на главную страницу сайта

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Здесь будут полезности
+
+## Установка:
+requirments + pip install uvicorn <br />
+Запуск локально: uvicorn src.main:app --reload
+
+## Чтоб запустить в интернете для теста:
+Сначала "npm install -g localtunnel" <br />
+Затем "lt --local-host 127.0.0.1 --port 8000" <br />
+Там выдаст ссылку, по которой надо будет ввести свой IP
--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 from src.pages.router import router as router_pages
 
 # uvicorn src.main:app --reload
-app = FastAPI()
+app = FastAPI(
+    title='TaskTaking'
+)
 app.include_router(router_pages)
 app.mount("/static", StaticFiles(directory='src/static'), name="static")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return RedirectResponse('pages/startpage', status_code=302)
