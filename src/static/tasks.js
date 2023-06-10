cdc148b5701ec8eebf05d1400ec921997f5c9996
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-06-10 16:16:40 +0500

    Появилась возможность взять и снять задачу

--- src/static/tasks.js
+++ src/static/tasks.js
@@ -1,17 +1,21 @@
 const modal = document.getElementById('modal');
 const modalTitle = document.getElementById('modal__title');
 const modalText = document.getElementById('modal__text');
+const modalButton = document.getElementById('button');
 
+let tasks = new Map();
+let takenTasks = new Set();
 
-let tasks = sendRequest("/tasks/get_tasks_by_teacher_id");
-
-tasks.then(data => createNewTasks(data.Data));
+let taskRequest = sendGetTaskRequest("/tasks/get_tasks_by_teacher_id")
+let takenTaskRequest = sendGetTaskRequest("/tasks/get_my_taken_tasks")
+    .then(data => data.Data.forEach(e => takenTasks.add(e.task_id)))
+taskRequest.then(data => createNewTasks(data.Data));
 
 function createNewTasks(data) {
-    data.forEach(e => createNewTask(e.name, e.contest_type + " " + e.contest_number, e.task_value,e.description));
+    data.forEach(e => createNewTask(e.name, `${e.contest_type} ${e.contest_number} ${e.task_number} задача` , e.task_value, e.description, e.id));
 }
 
-function createNewTask(tValue, pValue, gValue, dValue) {
+function createNewTask(tValue, pValue, gValue, dValue, id) {
     let newTask = document.createElement("div");
     newTask.className = "task_table";
     let title = document.createElement("div");
@@ -28,12 +32,31 @@ function createNewTask(tValue, pValue, gValue, dValue) {
     newTask.addEventListener('click', function (e) {
         modalTitle.innerHTML = tValue;
         modalText.innerHTML = dValue;
+        modalButton.innerHTML=takenTasks.has(id)?"Снять задачу":"Взять задачу"
         modal.classList.add('modal_active');
     });
+    addTaskToDictionary(tValue,id);
 }
 
-function sendRequest(url) {
+function addTaskToDictionary(name, id) {
+    tasks.set(name, id);
+}
+
+
+function sendGetTaskRequest(url) {
     return fetch(url).then(response => {
+        if (response.ok && response.status[0] !== "3") {
+            return response.json();
+        }
+    });
+}
+function sendTakeTaskRequest(url, id) {
+    return fetch(url+"?task_id="+id, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }})
+        .then(response => {
         if (response.ok && response.status[0] !== "3") {
             return response.json();
         }
@@ -41,6 +64,20 @@ function sendRequest(url) {
 }
 
 document.querySelector('.modal__close-button').addEventListener('click', function (e) {
-    let modal = document.getElementById('modal');
     modal.classList.remove('modal_active');
+});
+
+document.querySelector('.button').addEventListener('click', function (e) {
+    if (modalButton.innerHTML==="Снять задачу"){
+        modalButton.innerHTML = "Взять задачу"
+        taskId = tasks.get(modalTitle.innerHTML);
+        sendTakeTaskRequest("/tasks/drop_task", taskId);
+        takenTasks.delete(taskId)
+    }
+    else{
+    modalButton.innerHTML = "Снять задачу";
+    taskId = tasks.get(modalTitle.innerHTML);
+    sendTakeTaskRequest("/tasks/take_task",taskId);
+    takenTasks.add(taskId)
+    }
 });
