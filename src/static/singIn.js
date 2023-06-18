45e281d038963b891c4d058ad7c9ca94900986e1
jzxzz178 <pichuginf2016@mail.ru>
2023-06-18 22:01:04 +0500

    working on signin page 2

--- src/static/singIn.js
+++ src/static/singIn.js
@@ -3,7 +3,19 @@ const slider = document.querySelector(".slider");
 const loginBox = document.querySelector(".login-box");
 const signinBox = document.querySelector(".signin-box");
 const frame = document.querySelector('.frame');
+const isStudent = document.getElementById('isStudent');
+const teacherField = document.querySelector('.teacher-field');
+
 // let loginProcess = true;
+
+isStudent.onclick = function () {
+    if (isStudent.checked) {
+        slideToggle(teacherField, 500);
+    } else {
+        slideToggle(teacherField, 500, true);
+    }
+};
+
 buttons.forEach((btn, index) => {
     btn.addEventListener('click', () => {
         let start = null;
@@ -14,59 +26,76 @@ buttons.forEach((btn, index) => {
             // ВОЙТИ
             slider.style.left = `${index * 2000}px`;
             slider.style.width = "86px";
-            signinBox.classList.replace("active", "inactive");
             frame.classList.replace('frame-long', 'frame-short');
-
-            // alert(lgPos.top + ' ' + lgPos.right + ' ' + lgPos.bottom + ' ' + lgPos.left);
-            // let lgPos = loginBox.getBoundingClientRect();
-            //
-            // startPos = (lgPos.right + lgPos.left) / 2;
-            // endPos = -loginBox.offsetWidth;
-            // loginBox.classList.replace("inactive", "active");
+            // signinBox.classList.replace("active", "inactive");
+            slideToggle(signinBox, 500, true);
 
         } else {
             // ЗАРЕГАТЬСЯ
             slider.style.left = `${index * 108.5}px`;
             slider.style.width = "240px";
-            signinBox.classList.replace("inactive", "active");
             frame.classList.replace('frame-short', 'frame-long');
+            // signinBox.classList.replace("inactive", "active");
+            slideToggle(signinBox, 500);
 
-            // frame.classList.replace('frame-short', 'frame-long')
-            // let lgPos = loginBox.getBoundingClientRect();
-            startPos = getOffset(loginBox).left;
-            endPos = -loginBox.offsetWidth;
-            // loginBox.classList.replace("active", "inactive");
         }
-
-        // function step(timestamp) {
-        //     if (!start) start = timestamp;
-        //     let progress = timestamp - start;
-        //     let pos = Math.easeInOutQuad(progress, startPos, endPos - startPos, 2000);
-        //     loginBox.style.left = pos + 'px';
-        //
-        //     if (progress < 2000) {
-        //         window.requestAnimationFrame(step);
-        //     }
-        // }
-        //
-        // window.requestAnimationFrame(step);
     });
 });
 
-Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-};
+let animationFrameId = null;
 
-function getOffset(el) {
-    const rect = el.getBoundingClientRect();
-    return {
-        left: rect.left + window.scrollX,
-        top: rect.top + window.scrollY
-    };
+function slideToggle(element, speed, hide = false) {
+    let display = getComputedStyle(element).display;
+
+    if (!hide) {
+        // show element
+        element.style.display = 'block';
+        element.style.height = '0px';
+
+        let height = element.scrollHeight;
+
+        let step = height / (speed / 0.016);  // 0.016 - время выполнения одного кадра при 60fps
+
+        let animate = function () {
+            let currentHeight = parseFloat(getComputedStyle(element).height);
+            let newHeight = currentHeight + step;
+            element.style.height = `${newHeight}px`;
+
+            if (newHeight < height) {
+                animationFrameId = requestAnimationFrame(animate);
+            } else {
+                element.style.height = `${height}px`;  // прибиваем итоговый размер на случай небольшой погрешности при расчёте
+                cancelAnimationFrame(animationFrameId);
+            }
+        }
+
+        animationFrameId = requestAnimationFrame(animate);
+
+    } else {
+        // hide element
+        let height = element.scrollHeight;
+        let step = height / (speed / 0.016);
+
+        let animate = function () {
+            let currentHeight = parseFloat(getComputedStyle(element).height);
+            let newHeight = currentHeight - step;
+            element.style.height = `${newHeight}px`;
+
+            if (newHeight > 0) {
+                animationFrameId = requestAnimationFrame(animate);
+            } else {
+                element.style.display = 'none';
+                cancelAnimationFrame(animationFrameId);
+            }
+        }
+
+        animationFrameId = requestAnimationFrame(animate);
+    }
 }
+
+// Использование:
+// slideToggle(signinBox, 500); // где 500 это скорость анимации в миллисекундах
+
 
 // document.querySelector('.signin-nav .btn-top').addEventListener('click', function (e) {
 //     e.preventDefault();
