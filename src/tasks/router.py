ab0c2a4ff05db201550fbad286c61eeb0dfb7f68
JustASpace <js4std@gmail.com>
2023-06-23 02:32:50 +0500

    get_unchecked_task

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -20,12 +20,13 @@ current_user = fastapi_users.current_user()
 
 
 @router.get('/get_students_by_teacher_id')
-async def get_students_by_teacher_id(session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def get_students_by_teacher_id(session: AsyncSession = Depends(get_async_session),
+                                     user: User = Depends(current_user)):
     if user.role_id == 1:
-        query = select(user_table.c.username, user_table.c.score_sum).where(user_table.c.invited_by == user.id)\
+        query = select(user_table.c.username, user_table.c.score_sum).where(user_table.c.invited_by == user.id) \
             .order_by(user_table.c.score_sum)
     else:
-        query = select(user_table.c.username, user_table.c.score_sum).where(user_table.c.invited_by == user.invited_by)\
+        query = select(user_table.c.username, user_table.c.score_sum).where(user_table.c.invited_by == user.invited_by) \
             .order_by(user_table.c.score_sum)
 
     result = await session.execute(query)
@@ -37,36 +38,59 @@ async def get_students_by_teacher_id(session: AsyncSession = Depends(get_async_s
     }
 
 
-# @router.get('/get_unchecked_task')
-# # @cache(expire=3600)
-# async def get_students_by_teacher_id(session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
-#     if user.role_id == 1:
-#         raise Exception
-#
-#     query = select(user_table.c.username, user_table.c.score_sum).where(user_table.c.invited_by == user.invited_by)\
-#             .order_by(user_table.c.score_sum)
-#
-#     result = await session.execute(query)
-#
-#     return {
-#         'Status': 'Success',
-#         'Data': [r._asdict() for r in result],
-#         'Details': None
-#     }
+@router.get('/get_unchecked_task')
+async def get_unchecked_task(session: AsyncSession = Depends(get_async_session),
+                             user: User = Depends(current_user)):
+    if user.role_id == 1:
+        raise HTTPException(status_code=405, detail=
+        {
+            'Status': 'Error',
+            'Data': None,
+            'Details': 'Not a student'
+        })
+
+    if not user.has_unchecked_tasks:
+        return {
+            'Status': 'No unchecked tasks',
+            'Data': [],
+            'Details': None
+        }
+
+    query = select(task.c.id, task.c.name, task.c.task_value, taken_task.c.score) \
+        .join(taken_task, task.c.id == taken_task.c.task_id) \
+        .where(and_(taken_task.c.user_id == user.id, taken_task.c.is_checked == False))
+    checked_tasks = await session.execute(query)
+
+    await session.execute(update(taken_task) \
+                          .where(and_(taken_task.c.user_id == user.id, taken_task.c.is_checked == False)) \
+                          .values(is_checked=True))
+    await session.commit()
+
+    await session.execute(update(user_table)
+                          .where(user_table.c.id == user.id)
+                          .values(has_unchecked_tasks=False))
+    await session.commit()
+
+    return {
+        'Status': 'Success',
+        'Data': [r._asdict() for r in checked_tasks],
+        'Details': None
+    }
 
 
 @router.get('/get_tasks_by_teacher_id')
-async def get_tasks_by_teacher_id(session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def get_tasks_by_teacher_id(session: AsyncSession = Depends(get_async_session),
+                                  user: User = Depends(current_user)):
     try:
         current_dt = datetime.datetime.utcnow()
 
         if user.role_id == 1:
-            query = select(task)\
-                .where(and_(task.c.added_by == user.id, task.c.dead_line > current_dt))\
+            query = select(task) \
+                .where(and_(task.c.added_by == user.id, task.c.dead_line > current_dt)) \
                 .order_by(task.c.dead_line)
         else:
-            query = select(task)\
-                .where(and_(task.c.added_by == user.invited_by, task.c.dead_line > current_dt))\
+            query = select(task) \
+                .where(and_(task.c.added_by == user.invited_by, task.c.dead_line > current_dt)) \
                 .order_by(task.c.dead_line)
 
         result = await session.execute(query)
@@ -77,8 +101,7 @@ async def get_tasks_by_teacher_id(session: AsyncSession = Depends(get_async_sess
             'Details': None
         }
     except Exception:
-        raise HTTPException(status_code=500, detail=
-        {
+        raise HTTPException(status_code=500, detail={
             'Status': 'Error',
             'Data': None,
             'Details': None
@@ -86,7 +109,8 @@ async def get_tasks_by_teacher_id(session: AsyncSession = Depends(get_async_sess
 
 
 @router.get('/get_students_by_task_id')
-async def get_students_by_task_id(task_id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def get_students_by_task_id(task_id: int, session: AsyncSession = Depends(get_async_session),
+                                  user: User = Depends(current_user)):
     try:
         query = select(taken_task).where(taken_task.c.task_id == task_id)
         result = await session.execute(query)
@@ -120,8 +144,8 @@ async def get_my_taken_tasks(session: AsyncSession = Depends(get_async_session),
             'Details': 'Not a student'
         })
 
-    query = select(task.c.id, task.c.name, task.c.task_value, taken_task.c.score)\
-        .join(taken_task, task.c.id == taken_task.c.task_id)\
+    query = select(task.c.id, task.c.name, task.c.task_value, taken_task.c.score) \
+        .join(taken_task, task.c.id == taken_task.c.task_id) \
         .where(taken_task.c.user_id == user.id)
     result = await session.execute(query)
 
@@ -133,7 +157,8 @@ async def get_my_taken_tasks(session: AsyncSession = Depends(get_async_session),
 
 
 @router.post('/take_task')
-async def take_task(task_id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def take_task(task_id: int, session: AsyncSession = Depends(get_async_session),
+                    user: User = Depends(current_user)):
     if user.role_id != 2:
         raise HTTPException(status_code=405, detail=
         {
@@ -171,7 +196,9 @@ async def take_task(task_id: int, session: AsyncSession = Depends(get_async_sess
         })
 
     if len(task_dict) + 1 == max_task_cnt:
-        await session.execute(update(task).where(task.c.id == task_id).values(is_available=False))
+        await session.execute(update(task)
+                              .where(task.c.id == task_id)
+                              .values(is_available=False))
         await session.commit()
 
     return {
@@ -182,7 +209,8 @@ async def take_task(task_id: int, session: AsyncSession = Depends(get_async_sess
 
 
 @router.post('/drop_task')
-async def drop_task(task_id: int, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def drop_task(task_id: int, session: AsyncSession = Depends(get_async_session),
+                    user: User = Depends(current_user)):
     if user.role_id != 2:
         raise HTTPException(status_code=405, detail=
         {
@@ -212,7 +240,8 @@ async def drop_task(task_id: int, session: AsyncSession = Depends(get_async_sess
 
 
 @router.post('/add_task')
-async def add_task(new_task: TaskCreate, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
+async def add_task(new_task: TaskCreate, session: AsyncSession = Depends(get_async_session),
+                   user: User = Depends(current_user)):
     try:
         if user.role_id != 1:
             raise Exception
@@ -236,72 +265,50 @@ async def add_task(new_task: TaskCreate, session: AsyncSession = Depends(get_asy
 
 
 @router.post('/update_task')
-async def add_task(updated_task: TaskUpdate, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
-    # try:
-        if user.role_id != 1:
-            raise Exception
+async def add_task(updated_task: TaskUpdate, session: AsyncSession = Depends(get_async_session),
+                   user: User = Depends(current_user)):
+    if user.role_id != 1:
+        raise Exception
 
-        stmt = select(task).where(task.c.id == updated_task.task_id)
-        res = await session.execute(stmt)
-        task_dict = [r._asdict() for r in res]
+    stmt = select(task).where(task.c.id == updated_task.task_id)
+    res = await session.execute(stmt)
+    task_dict = [r._asdict() for r in res]
 
-        if user.id != task_dict[0]['added_by']:
-            raise Exception
+    if user.id != task_dict[0]['added_by']:
+        raise Exception
 
-        data = {}
-        data['name'] = updated_task.name
-        data['description'] = updated_task.description
-        data['taken_max'] = updated_task.taken_max
-        data['dead_line'] = task_dict[0]['dead_line'] + datetime.timedelta(days=updated_task.dead_line)
-        data['task_value'] = updated_task.task_value
+    data = {'name': updated_task.name, 'description': updated_task.description, 'taken_max': updated_task.taken_max,
+            'dead_line': task_dict[0]['dead_line'] + datetime.timedelta(days=updated_task.dead_line),
+            'task_value': updated_task.task_value}
 
-        stmt = update(task).values(data).where(task.c.id == updated_task.task_id)
-        await session.execute(stmt)
-        await session.commit()
+    stmt = update(task).values(data).where(task.c.id == updated_task.task_id)
+    await session.execute(stmt)
+    await session.commit()
 
-        return {'Status': 'Success'}
-    # except Exception:
-    #     raise HTTPException(status_code=405, detail=
-    #     {
-    #         'Status': 'Error',
-    #         'Data': None,
-    #         'Details': 'Not a teacher'
-    #     })
+    return {'Status': 'Success'}
 
 
 @router.post('/rate_task')
 async def rate_task(task_id: int, user_id: int, score: int,
                     session: AsyncSession = Depends(get_async_session), user: User = Depends(current_user)):
-    # try:
-        if user.role_id != 1:
-            raise Exception
+    if user.role_id != 1:
+        raise Exception
 
-        data = {}
-        data['task_id'] = task_id
-        data['user_id'] = user_id
-        data['score'] = score
-        data['is_checked'] = False
+    data = {'task_id': task_id, 'user_id': user_id, 'score': score, 'is_checked': False}
 
-        stmt = update(taken_task)\
-            .values(data)\
-            .where(and_(taken_task.c.task_id == task_id, taken_task.c.user_id == user_id))
+    stmt = update(taken_task) \
+        .values(data) \
+        .where(and_(taken_task.c.task_id == task_id, taken_task.c.user_id == user_id))
 
-        await session.execute(stmt)
-        await session.commit()
+    await session.execute(stmt)
+    await session.commit()
 
-        stmt = update(user_table) \
+    stmt = update(user_table) \
         .values({'has_unchecked_tasks': True,
                  'score_sum': user_table.c.score_sum + score}) \
         .where(user_table.c.id == user_id)
 
-        await session.execute(stmt)
-        await session.commit()
+    await session.execute(stmt)
+    await session.commit()
 
-        return {'Status': 'Success'}
-    # except Exception:
-    #     raise HTTPException(status_code=405, detail=
-    #     {
-    #         'Status': 'Error',
-    #         'Data': None,
-    #         'Details': 'Not a teacher'
-    #     })
+    return {'Status': 'Success'}
