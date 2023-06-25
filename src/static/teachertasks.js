653c9ada23ac4e76dac6eb76da5eda504da9a1c6
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-06-25 15:22:34 +0500

    Подтягиваются задачи преподавателя

--- src/static/teachertasks.js
+++ src/static/teachertasks.js
@@ -1,6 +1,10 @@
 const modal = document.getElementById('modal_form');
 const name = document.getElementById('modal_form');
 const form = document.forms.add;
+const tasks = document.getElementById("tasks")
+
+sendGetTaskRequest("/tasks/get_tasks_by_teacher_id").then(data => data.Data.forEach(e => makeTask(e)))
+
 document.getElementById('modal_form__close-button').addEventListener('click', function (e) {
     modal.classList.remove('modal_active');
 });
@@ -11,6 +15,24 @@ document.getElementById('add_task_button').addEventListener('click', function (e
 document.getElementById("button_save").addEventListener('click', function (e) {
     sendAddTaskRequest("/tasks/add_task")
 });
+
+function makeTask( data){
+    let newTask = document.createElement("tr");
+    newTask.id = data.id;
+    let name = document.createElement("td")
+    name.className = "teacher_task";
+    name.innerHTML = data.name;
+    let contest = document.createElement("td");
+    contest.innerHTML= `${data.contest_type} ${data.contest_number} задача ${data.task_number}`;
+    let score = document.createElement("td");
+    score.innerHTML=data.taken_max;
+    let student = document.createElement("td");
+    student.innerHTML = "Утка";
+    let input = document.createElement("td");
+    input.innerHTML = `<input className=grade type=number name=t min=0 max=30 step=0.1></td>`;
+    newTask.append(name, contest, score,student,input);
+    tasks.append(newTask);
+}
 
 function sendAddTaskRequest(url) {
     return fetch('/tasks/add_task', {
@@ -38,4 +60,11 @@ function sendAddTaskRequest(url) {
                 alert("Не удалось создать задачу, попробуйте еще раз")
             }
         });
+}
+function sendGetTaskRequest(url) {
+    return fetch(url).then(response => {
+        if (response.ok && response.status[0] !== "3") {
+            return response.json();
+        }
+    });
 }
