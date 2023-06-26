cf2e9bcb196df9a9ce853308f7a5dfe3f6f1d838
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-06-26 05:40:38 +0500

    Прикрутила изменение задачи

--- src/static/teachertasks.js
+++ src/static/teachertasks.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal_form');
 const modalTask = document.getElementsByClassName('modal_task')[0];
 const modalDelete = document.getElementsByClassName('modal_delete')[0];
+const modalChange = document.getElementById('modal_form_change');
 const name = document.getElementById('modal_form');
 const form = document.forms.add;
 const sortSign = document.getElementsByClassName('sort_sign')[0];
@@ -21,12 +22,22 @@ sendGetTaskRequest("/tasks/get_tasks_by_teacher_id").then(data => data.Data.forE
 document.getElementById('modal_form__close-button').addEventListener('click', function (e) {
     modal.classList.remove('modal_active');
 });
+document.getElementById('modal_form__close-button_change').addEventListener('click', function (e) {
+    modalChange.classList.remove('modal_active');
+});
+
 document.getElementById('add_task_button').addEventListener('click', function (e) {
     modal.classList.add('modal_active');
 });
+
 document.getElementById("button_delete").addEventListener('click', function (e) {
     modalDelete.classList.add('modal_active');
 });
+
+document.getElementById("button_edit").addEventListener('click', function (e) {
+    modalChange.classList.add('modal_active');
+});
+
 document.getElementById("button_send_mark").addEventListener('click', function (e) {
     [...document.getElementsByClassName("score")].forEach(e => {
         if (+e.value !== 0)
@@ -37,6 +48,7 @@ document.getElementById("button_send_mark").addEventListener('click', function (
 document.getElementById("button_delete_back").addEventListener('click', function (e) {
     modalDelete.classList.remove('modal_active');
 });
+
 document.getElementById("button_exactly_delete").addEventListener('click', function (e) {
     sendDeleteTaskRequest(modalTask.id)
 });
@@ -44,6 +56,10 @@ document.getElementById("button_exactly_delete").addEventListener('click', funct
 document.getElementById("button_save").addEventListener('click', function (e) {
     sendAddTaskRequest("/tasks/add_task")
 });
+document.getElementById("button_save_change").addEventListener('click', function (e) {
+    sendUpdateTaskRequest()
+});
+
 document.querySelector('.modal__close-button').addEventListener('click', function (e) {
     modalTask.classList.remove('modal_active');
 });
@@ -110,10 +126,31 @@ function makeTask(data) {
         modalTask.classList.add('modal_active');
         modalTask.id = data.id;
         modalLimit.innerHTML = `${data.taken_max} места до ${data.dead_line.slice(0, 10)} включительно`
+        let contest = document.getElementById("text_pair");
+        let contestValue=document.createElement("text");
+        contestValue.className="text_pair"
+        contestValue.innerHTML=data.contest_type;
+        contest.append(contestValue);
+        let number = document.getElementById("number");
+        let numberValue=document.createElement("text");
+        numberValue.className="text_pair"
+        numberValue.innerHTML=data.contest_number;
+        number.append(numberValue);
+        let word = document.getElementById("word");
+        let wordValue=document.createElement("text");
+        wordValue.className="text_pair"
+        wordValue.innerHTML=data.task_number;
+        word.append(wordValue);
+        document.forms.change.id = data.id;
+        document.forms.change.name.value=data.name;
+        document.forms.change.description.value=data.description;
+        document.forms.change.score.value=data.task_value;
+        document.forms.change.days.value=Math.ceil((Date.parse(data.dead_line) - Date.now()) / (1000 * 3600 * 24));;
     })
+
 }
 
-function sendAddTaskRequest(url) {
+function sendAddTaskRequest() {
     return fetch('/tasks/add_task', {
         method: "POST",
         headers: {
@@ -125,6 +162,28 @@ function sendAddTaskRequest(url) {
             contest_type: form.answer.value == "long" ? "Длинный" : "Короткий",
             contest_number: form.contestNumber.value,
             task_number: form.word.value,
+            description: form.description.value,
+            taken_max: form.limit.value,
+            dead_line: form.days.value,
+            task_value: form.score.value
+        })
+    }).then(response => {
+        if (response.ok && response.status[0] !== "3") {
+        } else {
+            alert("Не удалось создать задачу, попробуйте еще раз")
+        }
+    });
+}function sendUpdateTaskRequest() {
+    let form = document.forms.change;
+    return fetch('/tasks/update_task', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            task_id: form.id,
+            name:form.name.value ,
             description: form.description.value,
             taken_max: form.limit.value,
             dead_line: form.days.value,
